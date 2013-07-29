b9ce69e6cefe59ddbbe05e8e9ba1e74b8c7b3fa2
Raghu Betina <rbetina@gmail.com>
2013-07-29 08:12:48 -0500

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smdb::Application.config.secret_key_base = 'cc63da08b4960a2157a091e63333726ff29b27c0744573f353a17fd31aef13ca0bead148307b77e6fd0e8abda7fa1ebae71af7cb619811ef120b0dbfd25b87fa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smdb::Application.config.session_store :cookie_store, key: '_smdb_session'
