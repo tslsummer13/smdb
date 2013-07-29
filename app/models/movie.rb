6880f528d47b31269bcc424bd9f3c4d5183f95ed
Raghu Betina <rbetina@gmail.com>
2013-07-29 10:57:40 -0500

    Set up Movie#roles challenge

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ActiveRecord::Base
   def director
     return Director.find_by_id(self.director_id)
   end
+
+  def roles
+    # Make this work. It should return an array of all of the roles that belong to this movie.
+  end
 end
