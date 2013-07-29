f1bd3f7b56e820ada6d14e5e834dba78e11e72da
Raghu Betina <rbetina@gmail.com>
2013-07-29 11:17:25 -0500

    Updated role views. Set up Role#actor and Actor#roles challenge

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,2 +1,5 @@
 class Actor < ActiveRecord::Base
+  def roles
+    # Make this work. It should return an array of all the roles that belong to this actor.
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -2,4 +2,8 @@ class Role < ActiveRecord::Base
   def movie
     return Movie.find_by_id(self.movie_id)
   end
+
+  def actor
+    # Make this work. It should return the actor that this role belongs to.
+  end
 end
