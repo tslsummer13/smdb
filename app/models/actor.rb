4790ece153b76c195a823e6b45b9092597e63479
Raghu Betina <rbetina@gmail.com>
2013-07-29 11:32:53 -0500

    Completed Actor#roles association method

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,5 +1,5 @@
 class Actor < ActiveRecord::Base
   def roles
-    # Make this work. It should return an array of all the roles that belong to this actor.
+    return Role.where(:actor_id => self.id)
   end
 end
