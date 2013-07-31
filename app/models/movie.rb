8f0c75c582a4fcf4195a7d2e7b7ade8339f840ea
Raghu Betina <rbetina@gmail.com>
2013-07-31 09:15:11 -0500

    Replaced association methods

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,14 @@
 class Movie < ActiveRecord::Base
   # belongs to director
 
+
+
+  def director
+    return Director.find_by_id(self.director_id)
+  end
+
   # has many roles
+  def roles
+    return Role.where(:movie_id => self.id)
+  end
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,5 +1,11 @@
 class Role < ActiveRecord::Base
   # belongs to movie
+  def movie
+    return Movie.find_by_id(self.movie_id)
+  end
 
   # belongs to actor
+  def actor
+    return Actor.find_by_id(self.actor_id)
+  end
 end
