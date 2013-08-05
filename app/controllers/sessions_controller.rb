8c680837a92789edab7f54b349f2990a33f8ec79
Raghu Betina <rbetina@gmail.com>
2013-08-05 10:05:29 -0500

    Completed sign-in action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,14 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(:email => params[:email])
+    if user.present?
+      session[:user_id] = user.id
+      redirect_to root_url, :notice => "Welcome back, #{user.name}."
+    else
+      redirect_to new_session_url, :flash => { :error => "Something went wrong. Please try again." }
+    end
+  end
 end
