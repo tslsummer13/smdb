39ba9dbebbfcbf7defc0997c360c62954c030d73
Raghu Betina <rbetina@gmail.com>
2013-08-05 11:36:52 -0500

    Removed all sessions actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,17 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(:email => params[:email])
-    if user.present?
-      session[:user_id] = user.id
-      redirect_to root_url, :notice => "Welcome back, #{user.name}."
-    else
-      redirect_to new_session_url, :flash => { :error => "Something went wrong. Please try again." }
-    end
   end
 
   def destroy
-    reset_session
-    redirect_to root_url, :notice => "Signed out successfully."
   end
 end
