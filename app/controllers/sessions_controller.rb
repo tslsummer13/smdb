b7d3466bc7c1fbc1b824c56c3457e7ae9b3ff622
Raghu Betina <rbetina@gmail.com>
2013-08-05 11:41:12 -0500

    Added hints

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    # Find the user with the email address entered in the sign-in form
+    # If there is such a user,
+      # sign them in and
+      # redirect to root url
+    # If not,
+      # redirect back to sign-in form
   end
 
   def destroy
+    # Forget everything about the user
+    # Redirect back to home page
   end
 end
