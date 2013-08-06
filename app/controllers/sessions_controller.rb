8344f9bfec01ab9dc8b6c3ac38d957d367af52ab
Raghu Betina <rbetina@gmail.com>
2013-08-06 13:55:54 -0500

    Re-did sign-in logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,18 @@ class SessionsController < ApplicationController
 
   def create
     # Find the user with the email address entered in the sign-in form
+    user = User.find_by(:email => params[:email])
     # If there is such a user,
+    if user.present?
       # sign them in and
+      session[:user_id] = user.id
       # redirect to root url
+      redirect_to root_url
     # If not,
+    else
       # redirect back to sign-in form
+      redirect_to new_session_url
+    end
   end
 
   def destroy
