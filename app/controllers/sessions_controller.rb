ccbb0b95664755733692c1eab3ded114d548be16
Raghu Betina <rbetina@gmail.com>
2013-08-07 09:59:34 -0500

    Secured sign-in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(:email => params[:email])
-    if user.present? && user.password == params[:password]
+    if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "Welcome back, #{user.name}."
       redirect_to root_url #, :notice => "Welcome back, #{user.name}."
