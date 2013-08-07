e9596f11284b46a18d524eb3d311e5ce6fefa362
Raghu Betina <rbetina@gmail.com>
2013-08-07 10:09:56 -0500

    Clean-up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,8 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(:email => params[:email])
     if user.present? && user.authenticate(params[:password])
       session[:user_id] = user.id
-      flash[:notice] = "Welcome back, #{user.name}."
-      redirect_to root_url #, :notice => "Welcome back, #{user.name}."
+      redirect_to root_url, :notice => "Welcome back, #{user.name}."
     else
       flash.now[:error] = "Something went wrong. Please try again."
       render 'new'
@@ -15,10 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    # Forget everything about the user
     reset_session
-    # Redirect back to home page
-    flash[:notice] = "Signed out successfully."
-    redirect_to root_url
+    redirect_to root_url, :notice => "Signed out successfully."
   end
 end
