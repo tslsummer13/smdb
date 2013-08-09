734c68fa631418aaae914cf88898c94080eaf1df
Jeff Cohen <jeff@starterleague.com>
2013-08-09 09:35:40 -0500

    Add a custom validation to the Vote model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,34 +2,10 @@ class Movie < ActiveRecord::Base
   has_many :votes, :dependent => :destroy
   has_many :users, :through => :votes
 
-  # belongs to director
-
-  # The hand-written way
-  # def director
-  #   return Director.find_by_id(self.director_id)
-  # end
-
-  # The long Rails way
-  # belongs_to :director, :class_name => "Director", :foreign_key => "director_id"
-
-  # The short Rails way
   belongs_to :director
-
-  # has many roles
-  # def roles
-  #   return Role.where(:movie_id => self.id)
-  # end
   has_many :roles
 
   has_many :actors, :through => :roles
 
-  # def actors
-  #   my_actors = []
 
-  #   self.roles.each do |role|
-  #     my_actors << role.actor
-  #   end
-
-  #   return my_actors
-  # end
 end
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -4,4 +4,13 @@ class Vote < ActiveRecord::Base
 
   validates :user_id, :presence => true, :uniqueness => { :scope => :movie_id, :message => 'has already voted for that movie' }
   validates :movie_id, :presence => true
+
+  validate :user_can_only_vote_5_times
+
+  def user_can_only_vote_5_times
+    if self.user.votes.count >= 5
+      errors.add(:base, "You can't vote anymore.")
+    end
+  end
+
 end
