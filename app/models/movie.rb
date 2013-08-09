afdf4df57787713ffa0e143116574809d28d7d9a
Jeff Cohen <jeff@starterleague.com>
2013-08-09 08:57:02 -0500

    Use dependent: destroy to clean up data as needed

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,5 +1,5 @@
 class Movie < ActiveRecord::Base
-  has_many :votes
+  has_many :votes, :dependent => :destroy
   has_many :users, :through => :votes
 
   # belongs to director
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many :votes
+  has_many :votes, :dependent => :destroy
   has_many :movies, :through => :votes
 
   has_secure_password
