88edc168b54621eb98c417b20dc7b6b9cd2ee4fa
Vincent Cabansag <vcabansag@gmail.com>
2013-08-15 15:13:58 -0500

    Added authenticate method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < ActionController::Base
   end
 
   helper_method :current_user
+  before_filter :authenticate
+
+  def authenticate
+    if Rails.env.staging?
+      authenticate_or_request_with_http_basic do |username, password|
+        username == ENV["HTTP_USERNAME"] && password == ENV["HTTP_PASSWORD"]
+      end
+    end
+  end
+
 end
